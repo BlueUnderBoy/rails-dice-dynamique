0fe10fe580ba26774ab82e1586717422c8d396eb
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-28 08:00:30 +0000

    tous les fichiers ont met a jour

--- app/controllers/roll_controller.rb
+++ app/controllers/roll_controller.rb
@@ -4,7 +4,62 @@ class RollController < ApplicationController
   end
   
   def two_six
+    @rolls = []
+
+    2.times do
+      die = rand(1..6)
+
+      @rolls.push(die)
+    end
     render({:template => "roll_templates/two_six"})
+  end
+
+  def two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+
+      @rolls.push(die)
+    end
+    render({:template => "roll_templates/two_ten"}) 
+  end
+
+  def one_twenty
+    @rolls = []
+
+    1.times do
+      die = rand(1..20)
+  
+      @rolls.push(die)
+    end
+    render({:template => "roll_templates/one_twenty"})
+  end
+
+  def five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+
+      @rolls.push(die)
+    end
+    render({:template => "roll_templates/five_four"})
+  end
+
+  def flexible 
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    render({:template => "roll_templates/flexible"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get("/", {:controller => "roll", :action=> "home"})
   get("/dice/2/6", {:controller => "roll", :action=> "two_six"})
-  get("/paper", {:controller => "roll", :action=> "paper"})
-  get("/scissors", {:controller => "roll", :action=> "scissors"})
+  get("/dice/2/10", {:controller => "roll", :action=> "two_ten"})
+  get("/dice/1/20", {:controller => "roll", :action=> "one_twenty"})
+  get("/dice/5/4", {:controller => "roll", :action=> "five_four"})
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "roll", :action=> "flexible"})
   #can also put ":somethhing" for placeholder in route
 end
